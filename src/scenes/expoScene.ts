f30f4a95e6d37af34fc9aec8365ac29fd7b5fa19
EllenFerreira18 <ellen.github@gmail.com>
2024-06-12 17:18:41 -0300

    feat: colisão de objetos adicionado

--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,4 +1,4 @@
-import { Color, Engine, FadeInOut, Scene, Transition, vec } from "excalibur";
+import { Actor, CollisionType, Color, Engine, FadeInOut, Scene, Transition, vec } from "excalibur";
 import { Resources } from "../resources";
 import { Player } from "../actors/player";
 
@@ -27,5 +27,23 @@ export class expoScene extends Scene {
         jogador.z = 1
 
         this.add(jogador)
+
+        let camadaObjetosColisores = tiledMap.getObjectLayers("ObjetosColisores")[0]
+
+        console.log(camadaObjetosColisores)
+
+        camadaObjetosColisores.objects.forEach(objeto => {
+            const objetoAtual = new Actor({
+                name: objeto.name,
+                x: objeto.x + offsetX + (objeto.tiledObject.width! / 2),
+                y: objeto.y + offsetY + (objeto.tiledObject.height! / 2),
+                width: objeto.tiledObject.width,
+                height: objeto.tiledObject.height,
+                collisionType: CollisionType.Fixed,
+            })
+
+            this.add(objetoAtual)
+        })
+
     }
 }
